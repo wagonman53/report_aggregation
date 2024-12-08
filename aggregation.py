7cf5d94367fb748b137956b798edb5662849b4bf
wagon_man53 <mealgood@gmail.com>
2024-12-09 01:36:04 +0900

    #

--- aggregation.py
+++ aggregation.py
@@ -5,7 +5,7 @@ def calculate_group_means(df: pd.DataFrame, group_cols: List[str], mean_cols: Li
     result = df.groupby(group_cols,observed=True)[mean_cols].mean().reset_index()
     group_sizes = df.groupby(group_cols,observed=True).size().reset_index(name="count")
     total_rows = len(df)
-    group_sizes["frequency"] = (group_sizes["count"] / total_rows) * 100
+    group_sizes["%"] = (group_sizes["count"] / total_rows) * 100
     result = result.merge(group_sizes, on=group_cols)
     
     return result
