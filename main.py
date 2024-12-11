62f5bde73f501e35ccaf232c65e4c8368a4c57db
wagon_man53 <mealgood@gmail.com>
2024-12-11 22:09:40 +0900

    #

--- main.py
+++ main.py
@@ -27,13 +27,15 @@ df = categorical_feature.get_categorical_feature(df)
 with st.sidebar.form(key="form"):
     group_cols = st.multiselect("グループ変数の選択",feature_cols)
     rank_cols = st.multiselect("頻度を表示させる役の選択",utils.rank_list)
-    threshold = st.number_input(label="アクションの閾値",min_value=0,max_value=10,value=2,step=1)
+    threshold = st.number_input(label="サンプルの閾値",min_value=0,max_value=100,value=10,step=1)
+    action_threshold = st.number_input(label="アクションの閾値",min_value=0,max_value=10,value=2,step=1)
     submit = st.form_submit_button(label="実行")
 
 if submit: 
     mean_cols = utils.get_rank_action_col(action_cols,rank_cols)
 
     dfg = aggregation.calculate_group_means(df,group_cols,mean_cols)
+    dfg = dfg[dfg["count"] >= threshold]
     dfg = dfg.dropna(subset=mean_cols,how="all")
     dfg = dfg.round(1)
 
